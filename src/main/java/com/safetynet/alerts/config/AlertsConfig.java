09a81f2689dc347f1aad5a1c995b5a608e65e53e
xhxcx <xxhxcxx.sn@gmail.com>
2021-02-25 13:59:38 +0100

    add properties to manage actuators and add infos + add bean declaration for httptrace endpoint into AlertsConfig class

--- src/main/java/com/safetynet/alerts/config/AlertsConfig.java
+++ src/main/java/com/safetynet/alerts/config/AlertsConfig.java
@@ -3,6 +3,8 @@ package com.safetynet.alerts.config;
 import com.safetynet.alerts.model.AlertsData;
 import com.safetynet.alerts.utils.InputDataReader;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +24,16 @@ public class AlertsConfig {
     @Bean
     @PostConstruct
     public AlertsData getAlertsDatas(){return new InputDataReader().loadData(dataFilePath);}
+
+    /**
+     * Init HttpTraceRepository bean to use http trace actuator
+     *
+     * @return an InMemoryHttpTraceRepository
+     */
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 
 
 }
